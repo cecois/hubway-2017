3aa39f6037c94c425fba494c180c7e05d84eef78
cecois <cecmcgee@gmail.com>
2017-04-07 17:52:36 -0400

    station data manual

--- index.sh
+++ index.sh
@@ -1,17 +1,19 @@
 declare -a series=("01" "02" "03" "04" "05" "06" "07" "08" "09" "10" "11" "12")
 for item in ${series[@]};do
 # url="https://s3.amazonaws.com/hubway-data/2016${item}-hubway-tripdata.zip"
-	# echo "fetching $url..."
-	# curl -O $url ~/Downloads/hubway/
+# 	echo "fetching $url..."
+# 	curl -O $url
 
 # unzip "2016${item}-hubway-tripdata.zip"
-fil="data/2016${item}-hubway-tripdata.csv"
+
+
+fil="2016${item}-hubway-tripdata.csv"
 echo "running $fil..."
 
-
+cat $fil | tail -n+2 >> 2016-hubway-tripdata.csv
 # if [[ $item == "01" ]]; then
 
-	mongoimport -h localhost -d hubway -c rides --file $fil --type csv --headerline
+	# mongoimport -h localhost -d hubway -c rides --file $fil --type csv --headerline
 
 # else
 # 	mongoimport -h localhost -d hubway -c rides --file $fil --type csv --fields "tripduration,starttime,stoptime,start_station_id,start_station_name,start_station_latitude,start_station_longitude,end_station_id,end_station_name,end_station_latitude,end_station_longitude,bikeid,usertype,birth_year,gender"
